96b79098a682cc67967bc4080adae328e5c85680
MTrecozzi <47111522+MTrecozzi@users.noreply.github.com>
2019-09-05 12:35:48 -0400

    Added Apple Spawning and Apple Deletion

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -11,27 +11,48 @@ public class AppleTree : MonoBehaviour
     public float leftAndRightEdge = 10f;
     public float chanceToChangeDirections = 0.1f;
 
+    public float secondsBetweenAppleDrops = 3f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Invoke("DropApple", 2f);
     }
 
     // Update is called once per frame
     void Update()
     {
         // Basic Movement Spec
-     Vector3 pos = transform.position;
-     pos.x += speed * Time.deltaTime;
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
 
-     transform.position = pos;
+        transform.position = pos;
 
-     if ( pos.x < -leftAndRightEdge ) {                            
-           speed = Mathf.Abs(speed); 
-       } else if ( pos.x > leftAndRightEdge ) {                       
-           speed = -Mathf.Abs(speed); 
-       }
-        else if ( Random.value < chanceToChangeDirections ) {        // a
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);
+        }
+    }
+
+    void DropApple()
+    {                                                  // b
+
+        GameObject apple = Instantiate<GameObject>(applePrefab);      // c
+
+        apple.transform.position = transform.position;                  // d
+
+        Invoke("DropApple", secondsBetweenAppleDrops);                // e
+
+    }
+
+    void FixedUpdate()
+    {
+        if (Random.value < chanceToChangeDirections)
+        {        // a
             speed *= -1; // Change direction                           // b
         }
     }
