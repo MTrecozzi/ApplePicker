63a58638235d9b988968721d5990b586159fac9e
MTrecozzi <47111522+MTrecozzi@users.noreply.github.com>
2019-09-05 12:26:12 -0400

    Added Random and Border Turning

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -20,9 +20,19 @@ public class AppleTree : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Basic Movement Spec
      Vector3 pos = transform.position;
      pos.x += speed * Time.deltaTime;
 
-     transform.position = pos;   
+     transform.position = pos;
+
+     if ( pos.x < -leftAndRightEdge ) {                            
+           speed = Mathf.Abs(speed); 
+       } else if ( pos.x > leftAndRightEdge ) {                       
+           speed = -Mathf.Abs(speed); 
+       }
+        else if ( Random.value < chanceToChangeDirections ) {        // a
+            speed *= -1; // Change direction                           // b
+        }
     }
 }
